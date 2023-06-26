3bbb0ffbec7d6cb930a7532017363b24cb3a9ab5
1001 <1001@LAPTOP-UKTHHDV3>
2023-06-26 04:31:22 +0330

    Add project files.

---
+++ BMI/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace BMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblNormalRange = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCalculate.Font = new System.Drawing.Font("B Nazanin", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnCalculate.Location = new System.Drawing.Point(716, 113);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(157, 54);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "&محاسبه";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("B Nazanin", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnClear.Location = new System.Drawing.Point(716, 173);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(157, 53);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "&پاک کردن";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Font = new System.Drawing.Font("B Nazanin", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtHeight.Location = new System.Drawing.Point(183, 111);
+            this.txtHeight.Multiline = true;
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(283, 53);
+            this.txtHeight.TabIndex = 0;
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Font = new System.Drawing.Font("B Nazanin", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtWeight.Location = new System.Drawing.Point(183, 230);
+            this.txtWeight.Multiline = true;
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(283, 52);
+            this.txtWeight.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("B Nazanin", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnExit.Location = new System.Drawing.Point(716, 232);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(157, 55);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "&خروج";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("B Nazanin", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblResult.Location = new System.Drawing.Point(77, 340);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 52);
+            this.lblResult.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("B Nazanin", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(58, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 52);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "قد:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("B Nazanin", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(58, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 52);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "وزن:";
+            // 
+            // lblNormalRange
+            // 
+            this.lblNormalRange.AutoSize = true;
+            this.lblNormalRange.Font = new System.Drawing.Font("B Nazanin", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblNormalRange.Location = new System.Drawing.Point(81, 428);
+            this.lblNormalRange.Name = "lblNormalRange";
+            this.lblNormalRange.Size = new System.Drawing.Size(0, 52);
+            this.lblNormalRange.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(60, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 83);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("B Nazanin", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(509, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 44);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "کیلوگرم";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("B Nazanin", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(509, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 44);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "متر";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.CancelButton = this.btnCalculate;
+            this.ClientSize = new System.Drawing.Size(949, 543);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblNormalRange);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "شاخص توده ی بدنی";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblNormalRange;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ BMI/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double height = double.Parse(txtHeight.Text);
+                double weight = double.Parse(txtWeight.Text);
+                double bmi = weight / Math.Pow(height, 2);
+
+                double lowerLimit = 18.5 * Math.Pow(height, 2);
+                double upperLimit = 24.9 * Math.Pow(height, 2);
+
+                lblNormalRange.Text = $"محدوده ی وزنی مطلوب  : {lowerLimit:0.0}  تا  {upperLimit:0.0}  کیلوگرم است .";
+
+                string bmiCategory;
+                if (bmi < 18.5)
+                    bmiCategory = "کمبود وزن";
+                else if (bmi < 24.9)
+                    bmiCategory = "نرمال";
+                else if (bmi < 29.9)
+                    bmiCategory = "اضافه وزن";
+                else
+                    bmiCategory = "چاقی";
+
+                lblResult.Text = $"وضعیت فعلی شما در دسته ی {bmiCategory} است.";
+            }
+            catch {
+                DialogResult result = MessageBox.Show("شما هنوز فیلد های مورد نظر را پر نکرده اید!آیا می خواهید از برنامه خارج شوید ؟ ", "warning", MessageBoxButtons.YesNo);
+                if (result != DialogResult.Yes)
+                           Application.Exit();
+             }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (Control allcontrol in Controls)
+                {
+                    if (allcontrol is TextBox)
+                        ((TextBox)allcontrol).Clear();
+                    else if (allcontrol is Label && (allcontrol.Name == "lblNormalRange" || allcontrol.Name == "lblResult"))
+                        ((Label)allcontrol).Text = string.Empty;
+                }
+            }
+            catch
+            {
+                DialogResult result = MessageBox.Show("شما هنوز فیلد های مورد نظر را پر نکرده اید!آیا می خواهید از برنامه خارج شوید ؟ ", "warning", MessageBoxButtons.YesNo);
+                if (result != DialogResult.Yes)
+                    Application.Exit();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("آیا می خواهید از برنامه خارج شوید ؟", "Exit", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+   
+}
